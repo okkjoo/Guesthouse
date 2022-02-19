a92d31899f3dccb814b8436c9b8af2a8373d50df
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-02-19 19:32:35 +0800

    [delete]

--- src/pages/class/context/searchContext.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-const SearchContext = React.createContext()
-export default SearchContext
--- src/pages/class/lazy-load/demo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Demo() {
-  return <div>demo</div>
-}
-
-export default Demo
