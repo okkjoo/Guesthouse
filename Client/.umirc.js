ed7ed5de46537c6914e5194ec6ad8b89c837e2fe
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-08-24 17:39:00 +0800

    feat: Server in place of mock

--- Client/.umirc.js
+++ Client/.umirc.js
@@ -2,6 +2,13 @@
 export default {
   disableCSSModules: true,
   treeShaking: true,
+  mock: false,
+  proxy: {
+    '/api': {
+      target: 'http://127.0.0.1:7001/',
+      changeOrigin: true,
+    },
+  },
   routes: [
     {
       path: '/',
